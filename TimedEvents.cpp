103dd78cde21073aeb231b28839c3cc6cf2dcb47
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-31 16:09:04 -0500

    Add TimedEvent module base files.

---
+++ TimedEvents.cpp
@@ -0,0 +1,6 @@
+#include "TimedEvents.h"
+
+namespace sdds 
+{
+
+}
---
+++ TimedEvents.h
@@ -0,0 +1,14 @@
+#ifndef SDDS_TIMEDEVENTS_H
+#define SDDS_TIMEDEVENTS_H
+
+using namespace std;
+
+namespace sdds
+{
+	class TimedEvents
+	{
+	public:
+	};
+}
+
+#endif
