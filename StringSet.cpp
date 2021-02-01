69dd28fc8043c4bf535cc1fa34d79e3fc5694d24
pereiradaniel <danielpaulpereira@protonmail.com>
2021-01-31 20:34:34 -0500

    complete building, NOT WORKING YET!

--- StringSet.cpp
+++ StringSet.cpp
@@ -2,4 +2,93 @@
 
 namespace sdds {
 
+	StringSet::StringSet()
+	{
+		ss_strings = nullptr;
+		ss_string_count = 0;
+	}
+
+	StringSet::~StringSet()
+	{
+		delete[] ss_strings;
+		ss_strings = nullptr;
+	}
+
+	StringSet::StringSet(char *new_string)
+	{
+		string _b;
+		ifstream file(new_string);
+		int i = 0;
+		while (!file.eof())
+		{
+			getline(file, _b, " ");
+			++i;
+		}
+		ss_strings = new string[i + 1];
+		_b = {};
+		file.close();
+		file.open(new_string);
+		i = 0;
+		while (getline(file, _b, " "))
+		{
+			ss_strings[i] = _b;
+			++i;
+		}
+		ss_string_count = i;
+	}
+
+	StringSet::StringSet(const StringSet& input_string)
+	{
+		*this = input_string;
+	}
+
+	StringSet& StringSet::operator=(const StringSet& string)
+	{
+		ss_string_count = string.ss_string_count;
+		ss_strings = new string[ss_string_count];
+		for (int i = 0; i < ss_string_count; ++i)
+		{
+			ss_strings[i] = string.ss_strings[i];
+		}
+		return *this;
+	}
+
+	size_t StringSet::size()
+	{
+		return ss_string_count;
+	}
+
+	string StringSet::getEvent(int index)
+	{
+		if (ss_strings != nullptr)
+		{
+			return string(ss_strings[index]);
+		}
+		else
+		{
+			return "";
+		}
+	}
+
+	StringSet::StringSet(StringSet &&input_string)
+	{
+		*this = move(input_string);
+	}
+
+	StringSet& StringSet::operator=(StringSet&& input_string)
+	{
+		if (this != &input_string)
+		{
+			ss_string_count = input_string.ss_string_count;
+			ss_strings = new string[ss_string_count];
+			for (int i = 0; i < ss_string_count; ++i)
+			{
+				ss_strings[i] = input_string.ss_strings[i];
+			}
+			delete[] input_string.ss_strings;
+			input_string.ss_strings = nullptr;
+			input_string.ss_string_count = 0;
+		}
+		return *this;
+	}
 }
--- StringSet.h
+++ StringSet.h
@@ -2,6 +2,11 @@
 #define SDDS_STRINGSET_H
 
 #include <string>
+#include <iomanip>
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <fstream>
 
 using namespace std;
 
@@ -16,6 +21,8 @@ namespace sdds {
 		~StringSet();
 		StringSet(char* new_string);	// 1-argument constructor that receives the address of a c-style null terminated string containing the name of a file from which this member function populates the current object.
 		
+		StringSet(const StringSet &input_string);
+		StringSet& operator=(const StringSet &input_string);
 
 		size_t size();	// A query that returns the number of strings stored in the current object,
 		string getEvent(int index); // A query that returns a copy of the string at the index received.
